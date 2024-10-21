9283215c48c0054ac24d3cac094f2744e8ea8be8
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2024-10-21 18:32:58 +0200

    Update home_21.10.2024.sql

--- exercises/home_21.10.2024.sql
+++ exercises/home_21.10.2024.sql
@@ -28,3 +28,40 @@ INNER JOIN dane_zamowien ON zamowienia.NumerZamowienia = dane_zamowien.NumerZamo
 INNER JOIN produkty ON dane_zamowien.NumerProduktu = produkty.NumerProduktu
 WHERE produkty.NazwaProduktu LIKE "%kask";
 
+6)
+/*Pobierz imiona i nazwiska klientów wraz z nazwami zamówionych przez nich produktów.*/
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, produkty.NazwaProduktu
+FROM klienci
+INNER JOIN zamowienia ON klienci.IDKlienta = zamowienia.IDKlienta
+INNER JOIN dane_zamowien ON zamowienia.NumerZamowienia = dane_zamowien.NumerZamowienia
+INNER JOIN produkty ON dane_zamowien.NumerProduktu = produkty.NumerProduktu
+
+7)
+/*Wyświetl nazwę produktu oraz liczbę zamówień na ten produkt.*/
+SELECT produkty.NazwaProduktu, COUNT(dane_zamowien.NumerProduktu) AS LiczbaZamowien
+FROM produkty
+INNER JOIN dane_zamowien ON produkty.NumerProduktu = dane_zamowien.NumerProduktu
+GROUP BY produkty.NazwaProduktu DESC;
+
+8)
+/*Pobierz imiona i nazwiska pracowników, którzy obsługiwali zamówienia, wraz z numerami zamówień.*/
+SELECT pracownicy.NazwiskoPracownika, pracownicy.ImiePracownika, zamowienia.NumerZamowienia
+FROM pracownicy
+INNER JOIN zamowienia ON pracownicy.IDPracownika = zamowienia.IDPracownika
+GROUP BY zamowienia.NumerZamowienia
+
+9)
+/*Pobierz klientów, którzy złożyli najwięcej zamówień, oraz liczbę ich zamówień.*/
+SELECT klienci.ImieKlienta, klienci.NazwiskoKlienta, COUNT(zamowienia.IDKlienta) AS LiczbaZamowien
+FROM klienci
+INNER JOIN zamowienia ON klienci.IDKlienta = zamowienia.IDKlienta
+GROUP BY klienci.ImieKlienta, klienci.NazwiskoKlienta
+ORDER BY LiczbaZamowien DESC;
+
+10)
+/*Pobierz nazwę produktu i nazwę dostawcy, który dostarcza ten produkt*/
+SELECT produkty.NazwaProduktu, dostawcy.NazwaDostawcy
+FROM produkty
+INNER JOIN dostawcy_produktow ON produkty.NumerProduktu = dostawcy_produktow.NumerProduktu
+INNER JOIN dostawcy ON dostawcy_produktow.IDDostawcy = dostawcy.IDDostawcy
+
