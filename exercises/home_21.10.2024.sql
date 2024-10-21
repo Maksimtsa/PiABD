d72b9bea1d072f76a4b04b8723b1d5c898ce13c4
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2024-10-21 17:29:49 +0200

    Update home_21.10.2024.sql

--- exercises/home_21.10.2024.sql
+++ exercises/home_21.10.2024.sql
@@ -1,5 +1,19 @@
 1)wyświetl nazwę dostawcy
+SELECT dostawcy.NazwaDostawcy
+FROM dostawcy
+GROUP BY dostawcy.NazwaDostawcy;
 
 2)jakie sa produkty i ile kosztuja
+SELECT produkty.NazwaProduktu, produkty.CenaDetaliczna
+FROM produkty
+GROUP BY produkty.CenaDetaliczna DESC;
 
+3)podaj najdroższy produkt
+SELECT produkty.NazwaProduktu, MAX(produkty.CenaDetaliczna)
+FROM produkty
 
+4)podaj zamówienie założone ostatnio
+SELECT *
+FROM zamowienia
+GROUP BY zamowienia.DataZamowienia DESC
+LIMIT 1;
